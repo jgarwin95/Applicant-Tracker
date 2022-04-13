0ecfcb097b8dad1ba5de5b0c1a18856628a22280
Jacob <garwin2@illinois.edu>
2022-04-12 22:03:52 -0400

    add button to applicant card

--- src/features/applicantsPage/ApplicantCard.tsx
+++ src/features/applicantsPage/ApplicantCard.tsx
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import React from "react";
@@ -15,8 +17,29 @@ const ApplicantCard: React.FC<Props> = ({ session }) => {
           width: 1,
         }}
       >
-        {session.title}
-        {session.dateCreated}
+        <Box
+          sx={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              flexDirection: "column",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box>{session.title}</Box>
+            <Box>{session.dateCreated}</Box>
+          </Box>
+          <Box>
+            <Button variant="contained" size="medium">
+              Join Session
+            </Button>
+          </Box>
+        </Box>
       </Paper>
     </Grid>
   );
